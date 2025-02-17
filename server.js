0fa984516b85c9d0c5b94dfd0bf2273a4761893c
fabriciofabras <fabriciofabras@gmail.com>
2025-02-17 14:51:22 -0600

    Reservaciones

--- server.js
+++ server.js
@@ -2,13 +2,28 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from 'url';
+import { MongoClient } from 'mongodb';
+
+import dotenv from "dotenv";
 
 
 import { MercadoPagoConfig, Payment } from 'mercadopago';
 
+const MONGO_URI="mongodb+srv://fabriciofabras:FHern4nd3z.@travelcluster.am1gl.mongodb.net/?retryWrites=true&w=majority&appName=TravelCluster"
+
+// Función para conectar a la base de datos
+async function connectDB() {
+  const client = new MongoClient(MONGO_URI);
+  await client.connect();
+  console.log("Conectado a MongoDB");
+  return client.db("Travelfriends").collection("Reservations");
+}
+
+
+
+
 // const client = new MercadoPagoConfig({ accessToken: 'TEST-1634057283524851-012810-0bcf5b2ef2f2146d7a891106920a3a37-353348367', options: { timeout: 5000 } });
 const client = new MercadoPagoConfig({ accessToken: 'APP_USR-1634057283524851-012810-4581f90affdc1c47afc13aebc8c0d323-353348367', options: { timeout: 5000 } });
-const url = require('url');
 
 const payment = new Payment(client);
 
@@ -25,6 +40,28 @@ app.options('*', cors()); // Maneja preflight para cualquier endpoint
 // Configurar middleware y rutas
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Ruta GET para obtener todas las reservas
+app.get("/api/reservations", async (req, res) => {
+  try {
+    const collection = await connectDB();
+    const reservations = await collection.find().toArray();
+    res.json(reservations);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener las reservas" });
+  }
+});
+
+app.post("/api/reservation", async (req, res) => {
+  try {
+    const collection = await connectDB();
+    const newReservation = req.body; // Los datos vienen desde el frontend
+    const result = await collection.insertOne(newReservation);
+    res.status(201).json({ message: "Reservación creada", id: result.insertedId });
+  } catch (error) {
+    res.status(500).json({ error: "Error al crear la reservación" });
+  }
+});
 
 app.post('/process_payment', async (req, res) => {
  /*  res.setHeader('Access-Control-Allow-Origin', "https://travel-friends-mu.vercel.app");
