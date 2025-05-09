427402445381c5d4fca227741dabfad750491f83
fabriciofabras <fabriciofabras@gmail.com>
2025-05-09 13:37:44 -0600

    Update Server

--- server.js
+++ server.js
@@ -1,84 +1,35 @@
 import express from "express";
+import axios from "axios";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from 'url';
-import { MongoClient } from 'mongodb';
-
-import dotenv from "dotenv";
-
+import mercadopago from 'mercadopago';
 
 import { MercadoPagoConfig, Payment } from 'mercadopago';
 
-const MONGO_URI="mongodb+srv://fabriciofabras:FHern4nd3z.@travelcluster.am1gl.mongodb.net/?retryWrites=true&w=majority&appName=TravelCluster"
-
-// Función para conectar a la base de datos
-async function connectDB() {
-  const client = new MongoClient(MONGO_URI);
-  await client.connect();
-  console.log("Conectado a MongoDB");
-  return client.db("Travelfriends").collection("Reservations");
-}
-
-
-
-
-// const client = new MercadoPagoConfig({ accessToken: 'TEST-1634057283524851-012810-0bcf5b2ef2f2146d7a891106920a3a37-353348367', options: { timeout: 5000 } });
-const client = new MercadoPagoConfig({ accessToken: 'APP_USR-1634057283524851-012810-4581f90affdc1c47afc13aebc8c0d323-353348367', options: { timeout: 5000 } });
+const client = new MercadoPagoConfig({ accessToken: 'TEST-1634057283524851-012810-0bcf5b2ef2f2146d7a891106920a3a37-353348367', options: { timeout: 5000 } });
 
 const payment = new Payment(client);
 
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["POST", "GET"],
-  allowedHeaders: ["Content-Type"],
-}));
 
-app.options('*', cors()); // Maneja preflight para cualquier endpoint
+// Definir __dirname manualmente
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 // Configurar middleware y rutas
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
+app.use(cors());
 
-// Ruta GET para obtener todas las reservas
-app.get("/api/reservations", async (req, res) => {
-  try {
-    const collection = await connectDB();
-    const reservations = await collection.find().toArray();
-    res.json(reservations);
-  } catch (error) {
-    res.status(500).json({ error: "Error al obtener las reservas" });
-  }
-});
-
-app.post("/api/reservation", async (req, res) => {
-  try {
-    const collection = await connectDB();
-    const newReservation = req.body; // Los datos vienen desde el frontend
-    const result = await collection.insertOne(newReservation);
-    res.status(201).json({ message: "Reservación creada", id: result.insertedId });
-  } catch (error) {
-    res.status(500).json({ error: "Error al crear la reservación" });
-  }
-});
 
 app.post('/process_payment', async (req, res) => {
- /*  res.setHeader('Access-Control-Allow-Origin', "https://travel-friends-mu.vercel.app");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', false);
-  console.log("req", req.body); */
-  console.log('req')
-  console.log(req)
+  console.log("req", req.body);
 
-  console.log('req.body')
-  console.log(req.body)
-
-
-  console.log('request')
   const request = req.body;
-  console.log(request)
 
   try {
     const paymentResponse = await payment.create({
@@ -100,7 +51,7 @@ app.post('/process_payment', async (req, res) => {
     });
 
     console.log("Respuesta de Mercado Pago:", paymentResponse);
-
+    
     // Enviar la respuesta al frontend
     res.status(200).json({
       message: "Pago procesado con éxito",
@@ -114,6 +65,34 @@ app.post('/process_payment', async (req, res) => {
 });
 
 const port = 3000;
+
+const TRIPADVISOR_API_KEY = '519AAFC09925436194F4B5798A71F9A2';
+
+app.get('/api/hotels', async (req, res) => {
+  const query = req.query.q;
+
+  try {
+    const response = await axios.get(
+      `https://api.content.tripadvisor.com/api/v1/location/search?key=${TRIPADVISOR_API_KEY}&searchQuery=${encodeURIComponent(query)}&category=hotels`,
+      {
+        headers: {
+          accept: 'application/json',
+          origin: 'https://travel-friends-mu.vercel.app/',   // opcional si tu API key lo requiere
+          referer: 'https://travel-friends-mu.vercel.app/'   // opcional si tu API key lo requiere
+        }
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error al obtener datos de TripAdvisor:', error.response?.data || error.message);
+    res.status(500).json({ error: 'Error al consultar TripAdvisor' });
+  }
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
